a1889be5ebf76e50689878b97544295392391988
Jason Hamner <jason@Jasons-MacBook-Pro.local>
2017-10-09 20:27:21 -0600

    Added string, number, and date tests

--- DictionaryProxyTests/DictionaryProxyTests.swift
+++ DictionaryProxyTests/DictionaryProxyTests.swift
@@ -10,30 +10,19 @@ import XCTest
 @testable import DictionaryProxy
 
 class DictionaryProxyTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testString(){
+        let dict = DictionaryProxy(body: ["foo" : "bar"])
+        XCTAssert(dict["foo"].string == "bar")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testNumber(){
+        let dict = DictionaryProxy(body: ["foo" : 5])
+        XCTAssert(dict["foo"].number == 5)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    func test(){
-        XCTAssert(true)
+    func testDate(){
+        let time = NSDate()
+        let dict = DictionaryProxy(body: ["time" : time])
+        XCTAssert(dict["time"].date == time)
     }
 }
