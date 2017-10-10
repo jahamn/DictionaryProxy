44fa71bf0afff330c6177cddc681aa6deb96bfa0
Jason Hamner <jason@Jasons-MacBook-Pro.local>
2017-10-09 19:53:28 -0600

    Initial Commit

---
+++ DictionaryProxy/DictionaryProxy.h
@@ -0,0 +1,19 @@
+//
+//  DictionaryProxy.h
+//  DictionaryProxy
+//
+//  Created by Jason Hamner on 10/9/17.
+//  Copyright © 2017 Jason Hamner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DictionaryProxy.
+FOUNDATION_EXPORT double DictionaryProxyVersionNumber;
+
+//! Project version string for DictionaryProxy.
+FOUNDATION_EXPORT const unsigned char DictionaryProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DictionaryProxy/PublicHeader.h>
+
+
